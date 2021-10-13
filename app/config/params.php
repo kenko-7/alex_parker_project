5aa726d449fa6b7401d46f0544e4b03830de0f58
kenzo koeune <kenzokoeune.webdev@gmail.com>
2021-10-13 10:10:02 +0200

    Mise en place MVC

---
+++ app/config/params.php
@@ -0,0 +1,19 @@
+<?php 
+
+/* 
+        ./app/config/params.php
+*/
+
+// Paramètres de connexion
+        define('DB_HOST',     'localhost:3306');
+        define('DB_NAME',     'alex_parker_octobre_2021');
+        define('DB_USER',     'root');
+        define('DB_PASSWORD', 'root');
+
+// Autres constantes
+
+define('DATE_FORMAT', 'd-m-Y');
+
+// Initialisation des zones dynamiques
+        
+$content = '';
